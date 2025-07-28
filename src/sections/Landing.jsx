e92035242c917c58230a4ab94afb914fae37d2ed
Pratyush <choudharypratyush809@gmail.com>
2025-07-28 19:01:00 +0530

    testing

--- src/sections/Landing.jsx
+++ src/sections/Landing.jsx
@@ -18,7 +18,7 @@ const Landing = () => {
       {/* Background Video */}
       <div className="h-[62vh] w-full">
         <video
-          className="absolute md:block top-0 w-full left-0 object-cover"
+          className="absolute md:block h-[62vh] top-0 w-full left-0 object-cover"
           ref={videoRef}
           src="/images/Webvi.webm"
           autoPlay
@@ -27,7 +27,7 @@ const Landing = () => {
           playsInline
         />
         <video
-          className="absolute md:hidden top-0 w-full h-[64vh] left-0 object-cover"
+          className="absolute md:hidden top-0 w-full h-[70vh] left-0 object-cover"
           ref={videoRef}
           src="/images/mobile.webm"
           autoPlay
@@ -37,35 +37,51 @@ const Landing = () => {
         />
         <button
           onClick={toggleMute}
-          className="absolute z-20 top-4 right-4 bg-white/20 backdrop-blur-md text-white p-3 rounded-full hover:bg-white/30 transition shadow-md"
+          className="absolute z-20 top-4 right-4 bg-white/20 backdrop-blur-md text-black p-2 md:p-3 rounded-full hover:bg-white/30 transition shadow-md"
         >
-          {isMuted ? <FaVolumeMute size={24} /> : <FaVolumeUp size={24} />}
+          {isMuted ? <FaVolumeMute size={18} /> : <FaVolumeUp size={18} />}
         </button>
       </div>
 
       {/* <Scroll /> */}
-      <div className="animation w-full">
+      <div className="animation mt-14 md:mt-0 w-full">
         <div className="relative text flex whitespace-nowrap overflow-hidden">
           <motion.h1
             initial={{ x: 0 }}
             animate={{ x: "-100%" }}
-            transition={{ ease: "linear", repeat: Infinity, duration: 16 }}
+            transition={{ ease: "linear", repeat: Infinity, duration: 6 }}
+            md:transition={{ ease: "linear", repeat: Infinity, duration: 16 }}
             className="text-7xl font-bold"
           >
-            <span className="flex justify-center items-center ml-40 text-white [text-shadow:_-2px_-2px_0_#0a0322,2px_2px_0_#0a0322,-2px_2px_0_#0a0322,2px_-2px_0_#0a0322]">
-              <img className="h-40 w-40" src="/logo.PNG" alt="" />
-              Explore every destination, Every season – with FriskyTrails!
+            <span className="flex justify-center items-center ml-20 md:ml-40 text-white [text-shadow:_-2px_-2px_0_#0a0322,2px_2px_0_#0a0322,-2px_2px_0_#0a0322,2px_-2px_0_#0a0322]">
+              <img
+                className="h-20 w-20 md:h-40 md:w-40"
+                src="/logo.PNG"
+                alt=""
+              />
+              {/* Explore every destination, Every season – with FriskyTrails! */}
+              <h1 className="md:text-6xl text-3xl">
+                Explore every destination, Every season – with FriskyTrails!
+              </h1>
             </span>
           </motion.h1>
           <motion.h1
             initial={{ x: 0 }}
             animate={{ x: "-100%" }}
-            transition={{ ease: "linear", repeat: Infinity, duration: 16 }}
+            transition={{ ease: "linear", repeat: Infinity, duration: 6 }}
+            md:transition={{ ease: "linear", repeat: Infinity, duration: 16 }}
             className="text-7xl font-bold"
           >
-            <span className="flex justify-center items-center ml-40 text-white [text-shadow:_-2px_-2px_0_#0a0322,2px_2px_0_#0a0322,-2px_2px_0_#0a0322,2px_-2px_0_#0a0322]">
-              <img className="h-40 w-40" src="/logo.PNG" alt="" />
-              Explore every destination, Every season – with FriskyTrails!
+            <span className="flex justify-center items-center ml-20 md:ml-40 text-white [text-shadow:_-2px_-2px_0_#0a0322,2px_2px_0_#0a0322,-2px_2px_0_#0a0322,2px_-2px_0_#0a0322]">
+              <img
+                className="h-20 w-20 md:h-40 md:w-40"
+                src="/logo.PNG"
+                alt=""
+              />
+              {/* Explore every destination, Every season – with FriskyTrails! */}
+              <h1 className="md:text-6xl text-3xl">
+                Explore every destination, Every season – with FriskyTrails!
+              </h1>
             </span>
           </motion.h1>
         </div>
