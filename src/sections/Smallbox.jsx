7710a8ef094b0b5bbccfc6e639a7deed223544dc
Pratyush <choudharypratyush809@gmail.com>
2025-10-02 16:08:54 +0530

    travel destinations

--- src/sections/Smallbox.jsx
+++ src/sections/Smallbox.jsx
@@ -1,60 +1,42 @@
 const Smallbox = () => {
-    const data = [
-        {
-            name: "ASIA",
-            image: "/blogimages/trip.png"
-        },
-        {
-            name: "ASIA",
-            image: "/images/rock.jpg"
-        },
-        {
-            name: "ASIA",
-            image: "/images/rock.jpg"
-        },
-        {
-            name: "ASIA",
-            image: "/images/rock.jpg"
-        },
-        {
-            name: "ASIA",
-            image: "/images/rock.jpg"
-        },
-        {
-            name: "ASIA",
-            image: "/images/rock.jpg"
-        },
-        {
-            name: "ASIA",
-            image: "/images/rock.jpg"
-        },
-        {
-            name: "ASIA",
-            image: "/images/rock.jpg"
-        },
-        {
-            name: "ASIA",
-            image: "/images/rock.jpg"
-        },
-    ]
+  const data = [
+    { name: "Goa", image: "/destinations/Goa.png" },
+    { name: "Himachal", image: "/destinations/Himachal.png" },
+    { name: "Kashmir", image: "/destinations/Kashmir.png" },
+    { name: "Tawang", image: "/destinations/Tawang.png" },
+    { name: "Kerala", image: "/destinations/Kerala.png" },
+    { name: "Andaman", image: "/destinations/Andaman.png" },
+    { name: "Meghalaya", image: "/destinations/Meghalaya.png" },
+    { name: "Rajasthan", image: "/destinations/Rajasthan.png" },
+    { name: "Uttarakhand", image: "/destinations/Uttarakhand.png" },
+  ];
+
   return (
     <>
-    {
-        data.map((item, index) => (
-       <div key={index} className="img h-[12vh] w-[27vw] md:w-[6vw] bg-green-600">
-            <div className='flex justify-center items-center' style={{
-                height: "100%",
-                width: "100%",
-                backgroundSize: "cover",
-                backgroundImage: `url(${item.image})`,
-            }}>
-                <h3 className='text-white tracking-tighter text-sm font-bold'>{item.name}</h3>
-            </div>
-       </div>
-        ))
-    }
-    </>
-  )
-}
+      {data.map((item, index) => (
+        <div
+          key={index}
+          className="relative overflow-hidden h-[12vh] w-[27vw] md:w-[6vw] md:rounded-md"
+          style={{
+            backgroundImage: `url(${item.image})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          {/* Black overlay */}
+          <div className="absolute inset-0 bg-black/30 md:bg-black/20"></div>
 
-export default Smallbox
+          {/* Centered text */}
+          <div className="relative flex justify-center items-center h-full w-full">
+            <h3 className="text-white tracking-tighter text-xs font-bold uppercase">
+              {item.name}
+            </h3>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Smallbox;
