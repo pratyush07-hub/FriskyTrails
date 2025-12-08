b2026b18070658400140a3cef65a822e1a9a5b1c
Harsh rawat <hr7399894@gmail.com>
2025-12-09 00:16:06 +0530

    Resolve merge conflict: keep testimonialData.js file

---
+++ src/lib/testimonialData.js
@@ -0,0 +1,43 @@
+export const testimonialData = [
+  {
+    name: "Ashutosh Pareek",
+
+    image: "/feedback/ashutosh.png",
+    review: "If you're looking to experience the Himalayas in the most authentic and beautiful way, Hampta Pass with FriskyTrails is the way to go. Thank you, team, for an unforgettable journey. I'll definitely be back for more adventures with you guys!",
+    rating: 5
+  },
+  {
+    name: "Gokul Ram",
+
+    image: "/feedback/gokul.png",
+    review: "They did all the arrangements as per our needs in Goa and it was correctly priced and the communication was neat. Will recommend to others.",
+    rating: 4
+  },
+  {
+    name: "Harsh Choudhary",
+ 
+    image: "/feedback/harsh-chaudhary.avif",
+    review: "Thank you so much for your kind words! 🌟 We're thrilled to hear that you had a wonderful holiday with FriskyTrails",
+    rating: 5
+  },
+  {
+    name: "Nidhi Jaiswal",
+
+    image: "/feedback/nidhi.png",
+    review: "Such an amazing journey with Frisky Trails! The Hampta Pass trek had its challenges, but the crew made it so much fun and comfortable. They were helpful throughout, and the group vibes were just perfect.",
+    rating: 5
+  },
+  {
+    name: "Sahu VijayLaxmi",
+
+    image: "/feedback/sahu-vijayLakshmi.avif",
+    review: "The agency was incredibly responsive to our needs, even with last-minute adjustments, and their passion for the region shone through in every recommendation.It was really a fantastic experience",
+    rating: 4
+  },
+  {
+    name: "Manoj R Gupta",
+    image: "/feedback/Manoj-R-Gupta.avif",
+    review: "It was a wonderful and fabulous holiday with FriskyTrails. We went to Goa, such an amazing place to visit.",
+    rating: 5
+  }
+]
